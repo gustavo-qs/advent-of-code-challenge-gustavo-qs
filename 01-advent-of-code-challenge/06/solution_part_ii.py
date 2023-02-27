dbe59f5fbc63423dfaca7c313c04a9c8d58e99d6
Gustavo <gustavo@termoplex.com.br>
2023-02-26 22:43:45 -0300

    day 6 pt2

--- 01-advent-of-code-challenge/06/solution_part_ii.py
+++ 01-advent-of-code-challenge/06/solution_part_ii.py
@@ -1,45 +1,16 @@
 import sys
 
-grid = []
-col = { 1: 1, 2: 5, 3: 9, 4: 13, 5: 17, 6: 21, 7: 25, 8: 29, 9: 33 }
-aux = True
+def message_start(datastream):
+    buffer = ''
+    for i, char in enumerate(datastream):
+        buffer += char
+        if len(buffer) > 14:
+            buffer = buffer[1:]
+        if len(buffer) == 14 and len(set(buffer)) == 14:
+            return i + 1
+    return -1  
 
-for line in sys.stdin:
-  if(line.strip() and aux):
-    grid.append([char for char in line])
-    inverted_grid = list(map(list, zip(*grid)))
-    inverted_grid = [l[:-1] for l in inverted_grid]
-    inverted_grid = [[c for c in row if c != ' '] for row in inverted_grid]
+stream = sys.stdin.readlines()
 
-  else: 
-    if(not line.strip()):
-      aux = False
-      continue
-    else:
-      move_qty = int(line.split()[1])
-      move_from = int(line.split()[3])
-      move_to = int(line.split()[5])
-
-      if(move_qty == 1):
-        for index, crate in enumerate(inverted_grid[col[move_from]][:move_qty]):
-          inverted_grid[col[move_to]].insert(0, crate)         
-          inverted_grid[col[move_from]][index] = ' '
-
-        inverted_grid = [[c for c in row if c != ' '] for row in inverted_grid]
-      else:        
-        crates = []
-        for i in range(0, move_qty):
-          crates.append(inverted_grid[col[move_from]][i])
-        
-        for index, crate in enumerate(crates[::-1]):
-          inverted_grid[col[move_to]].insert(0, crate)
-          inverted_grid[col[move_from]][index] = ' '
-
-        inverted_grid = [[c for c in row if c != ' '] for row in inverted_grid]
-
-end_crates = ''
-
-for i in range(1,10):
-  end_crates += inverted_grid[col[i]][0]
-
-print(end_crates)
+start = message_start(stream[0])
+print(start)
