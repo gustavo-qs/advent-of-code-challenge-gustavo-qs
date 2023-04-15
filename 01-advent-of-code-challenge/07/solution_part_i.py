30967995aa89d3ef47fac2901f1db682d7d5d440
Gustavo Queiroz <compgustavo@edu.unifil.br>
2023-04-15 12:13:22 -0300

    day 7 pt 1 and 2 straight up sufferring

---
+++ 01-advent-of-code-challenge/07/solution_part_i.py
@@ -0,0 +1,34 @@
+import sys
+
+directories = {"/": 0}
+current_path = "/"
+
+for command in sys.stdin:
+  if command[0] == "$":
+      if command[2:4] == "ls":
+        pass
+      elif command[2:4] == "cd":
+        if command[5:6] == "/":
+          current_path = "/"
+        elif command[5:7] == "..":
+          current_path = current_path[0:current_path.rfind("/")]
+        else:
+          dir_name = command[5:]
+          current_path = current_path + "/" + dir_name
+          directories.update({current_path: 0})
+  elif command[0:3] == "dir":
+    pass
+  else:
+    file_size = int(command[:command.find(" ")])
+    dir = current_path
+    for _ in range(current_path.count("/")):
+      directories[dir] += file_size
+      dir = dir[:dir.rfind("/")]
+
+total = 0
+
+for dir in directories:
+  if directories[dir] < 100000:
+    total += directories[dir]
+
+print(total)
---
+++ 01-advent-of-code-challenge/07/solution_part_ii.py
@@ -0,0 +1,32 @@
+import sys
+
+directories = {"/": 0}
+current_path = "/"
+
+for command in sys.stdin:
+    if command[0] == "$":
+        if command[2:4] == "ls":
+            pass
+        elif command[2:4] == "cd":
+            if command[5:6] == "/":
+                current_path = "/"
+            elif command[5:7] == "..":
+                current_path = current_path[0:current_path.rfind("/")]
+            else:
+                dir_name = command[5:]
+                current_path = current_path + "/" + dir_name
+                directories.update({current_path: 0})
+    elif command[0:3] == "dir":
+        pass
+    else:
+        file_size = int(command[:command.find(" ")])
+        dir = current_path
+        for _ in range(current_path.count("/")):
+            directories[dir] += file_size
+            dir = dir[:dir.rfind("/")]
+
+limit = 30000000 - (70000000 - directories["/"])
+valid_dirs = [dir for dir in directories if directories[dir] > limit]
+smallest = min(valid_dirs, key=directories.get)
+
+print(directories[smallest])
